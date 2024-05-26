258ef7ef97c029baefa1d106eecfcddeddedac93
Lucas Daniel <daniellucasadebayo@gmail.com>
2024-05-26 11:50:37 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -21,13 +21,19 @@ def main():
 
     # sends http status message or code
     # client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-    response :bytes = "HTTP/1.1 200 OK\r\n\r\n".encode()
 
     """
     we're checking if the path extracted from the HTTP request is anything other than the root path '/'. We do this by splitting the first line of the request data on spaces and looking at the second element, which is the path.
     If the path is not '/', we set the response variable to a string that represents a 404 Not Found HTTP response. This string is then encoded into bytes, which is necessary because network communication is done using bytes rather than strings. If the path is '/', the response remains as the 200 OK response set earlier in the code
     """
-    if request_data[0].split(" ")[1] != '/':
+    path = request_data[0].split(" ")[1]
+    print(f"Path is {path}")
+    if path == '/':
+        response :bytes = "HTTP/1.1 200 OK\r\n\r\n".encode()
+    elif path.startswith('/echo'):
+        unknown_path = path[6:]
+        response :bytes = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(unknown_path)}\r\n\r\n{unknown_path}".encode()
+    else:
         response :bytes = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
     
     client.send(response)
