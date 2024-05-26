a1c99d29f6d11451407b80873592b65d0cf4e2b4
Lucas Daniel <daniellucasadebayo@gmail.com>
2024-05-26 12:38:49 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221))
     client, address = server_socket.accept() # wait for client
-    print(f"Accepted connection from {address}")
+    print(f"Accepted connection from {address}\n")
     
     """
     we're receiving data from the client using the recv method on the client socket. 
@@ -18,7 +18,7 @@ def main():
     which converts it to a format we can work with in Python."""
     data :str = client.recv(1024).decode()
     request_data :list[str] = data.split('\r\n')
-
+    
     # sends http status message or code
     # client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
@@ -27,9 +27,16 @@ def main():
     If the path is not '/', we set the response variable to a string that represents a 404 Not Found HTTP response. This string is then encoded into bytes, which is necessary because network communication is done using bytes rather than strings. If the path is '/', the response remains as the 200 OK response set earlier in the code
     """
     path = request_data[0].split(" ")[1]
-    print(f"Path is {path}")
+    print(f"Path is {path}\n")
     if path == '/':
         response :bytes = "HTTP/1.1 200 OK\r\n\r\n".encode()
+    elif path == '/user-agent':
+        print(request_data)
+        user_agent = [data for data in request_data if data[:5] == 'User-'][0].split(" ")[1]
+        print(user_agent)
+        # user_agent = request_data[2].split(" ")[1]
+        response :bytes = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode()
+    
     elif path.startswith('/echo'):
         unknown_path = path[6:]
         response :bytes = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(unknown_path)}\r\n\r\n{unknown_path}".encode()
